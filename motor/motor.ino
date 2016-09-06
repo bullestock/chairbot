3e4e90ddf5dcb2af10a1b1587975bb8291843437
Torsten Martinsen <torsten@bullestock.net>
2016-09-06 22:35:08 +0200

    Use correct PWM pins; add V command.

--- motor/motor.ino
+++ motor/motor.ino
@@ -2,18 +2,19 @@
 
 const int INTERNAL_LED = 13;
 const int R_PWM_A = 5;
-const int L_PWM_A = 4;
+const int L_PWM_A = 3;
 const int R_EN_A = 6;
-const int L_EN_A = 3;
+const int L_EN_A = 4;
 const int R_IS_A = 7;
 const int L_IS_A = 2;
-const int R_PWM_B = A1;
+const int R_PWM_B = 9;
 const int L_PWM_B = 11;
 const int R_EN_B = A2;
 const int L_EN_B = 12;
 const int R_IS_B = A3;
 const int L_IS_B = 13;
 const int BRAKE = 8;
+const int V_SENSE = A0;
 const int BUF_SIZE = 200;
 const int MAX_IDLE_COUNT = 20000;
 
@@ -38,6 +39,7 @@ void setup()
     pinMode(R_IS_B, INPUT);
 
     pinMode(BRAKE, OUTPUT);
+    pinMode(V_SENSE, INPUT);
 
     digitalWrite(R_PWM_A, LOW);
     digitalWrite(L_PWM_A, LOW);
@@ -82,16 +84,11 @@ void set_power(// 0-1
     {
         analogWrite(l_pwm_pin, power);
         analogWrite(r_pwm_pin, 0);
-        Serial.print("L ");
-        Serial.print(power);
-        Serial.println(" R 0");
     }
     else
     {   
         analogWrite(l_pwm_pin, 0);
         analogWrite(r_pwm_pin, -power);
-        Serial.print("L 0 R ");
-        Serial.println(power);
     }
 }
 
@@ -160,6 +157,11 @@ void process(const char* buffer)
             Serial.println("OK");
         }
         return;
+
+    case 'v':
+    case 'V':
+        Serial.println(analogRead(V_SENSE)/1023.0*5*4);
+        break;
         
     default:
         Serial.print("Error: Unknown command '");
@@ -171,118 +173,24 @@ void process(const char* buffer)
 
 void run_test()
 {
-    for (int i = 0; i < 256; i++)
+    for (int i = -255; i < 256; i++)
     {
         Serial.print("A ");
         Serial.println(i);
         set_power(0, i);
-        delay(200);
+        delay(100);
     }
     set_power(0, 0);
     delay(2000);
-    for (int i = 0; i < 256; i++)
+    for (int i = -255; i < 256; i++)
     {
         Serial.print("B ");
         Serial.println(i);
         set_power(1, i);
-        delay(200);
+        delay(100);
     }
     set_power(1, 0);
-
-#if 0
-    const int DELTA = 80;
-    const int MAX_PWR = 128+DELTA;
-    const int MIN_PWR = 128-DELTA;
-
-    digitalWrite(BRAKE, HIGH);
-
-    Serial.println("Motor A");
-    digitalWrite(INTERNAL_LED, HIGH);
-    delay(100);
-    digitalWrite(INTERNAL_LED, LOW);
-    delay(100);
-    digitalWrite(INTERNAL_LED, HIGH);
-    delay(100);
-    digitalWrite(INTERNAL_LED, LOW);
-    delay(100);
-    analogWrite(R_PWM_A, 0);
-    analogWrite(L_PWM_A, 0);
-    digitalWrite(R_EN_A, HIGH);
-    digitalWrite(L_EN_A, HIGH);
-    delay(1000);
-    for (int i = 0; i < 256; i++)
-    {
-        Serial.print("A L ");
-        Serial.println(i);
-        analogWrite(R_PWM_A, i);
-        delay(1000);
-    }
-    delay(1000);
-    for (int i = MIN_PWR; i < MAX_PWR; i++)
-    {
-        Serial.print("A R ");
-        Serial.println(i);
-        analogWrite(L_PWM_A, i);
-        delay(100);
-    }
-    Serial.println("Off");
-
-    Serial.println("Disable");
-    digitalWrite(R_EN_A, LOW);
-    digitalWrite(L_EN_A, LOW);
-    Serial.println("Brake on");
-    delay(500);
-    digitalWrite(BRAKE, LOW);
-
-    delay(5000);
-    Serial.println("Motor B");
-    digitalWrite(INTERNAL_LED, HIGH);
-    delay(100);
-    digitalWrite(INTERNAL_LED, LOW);
-    delay(100);
-    digitalWrite(INTERNAL_LED, HIGH);
-    delay(100);
-    digitalWrite(INTERNAL_LED, LOW);
-    delay(100);
-    digitalWrite(INTERNAL_LED, HIGH);
-    delay(100);
-    digitalWrite(INTERNAL_LED, LOW);
-    delay(100);
-    digitalWrite(INTERNAL_LED, HIGH);
-    delay(100);
-    digitalWrite(INTERNAL_LED, LOW);
-    delay(100);
-    digitalWrite(INTERNAL_LED, HIGH);
-    delay(100);
-    digitalWrite(INTERNAL_LED, LOW);
-    delay(100);
-    digitalWrite(INTERNAL_LED, HIGH);
-    delay(100);
-    digitalWrite(INTERNAL_LED, LOW);
-    delay(100);
-    digitalWrite(BRAKE, HIGH);
-    analogWrite(R_PWM_B, 0);
-    analogWrite(L_PWM_B, 0);
-    digitalWrite(R_EN_B, HIGH);
-    digitalWrite(L_EN_B, HIGH);
-    delay(1000);
-    for (int i = MIN_PWR; i < MAX_PWR; i++)
-    {
-        Serial.print("Power ");
-        Serial.println(i);
-        analogWrite(R_PWM_B, i);
-        analogWrite(L_PWM_B, 255-i);
-        delay(100);
-    }
-    Serial.println("Off");
-    analogWrite(R_PWM_B, 0);
-    analogWrite(L_PWM_B, 0);
-    digitalWrite(R_EN_B, LOW);
-    digitalWrite(L_EN_B, LOW);
-    digitalWrite(BRAKE, LOW);
-    digitalWrite(INTERNAL_LED, LOW);
-#endif
-    delay(5000);
+    Serial.println("Test done");
 }
 
 int index = 0;
