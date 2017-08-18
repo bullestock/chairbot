09d11e40e7c5c8929ce9bda4ede2115775de6005
Torsten Martinsen <torsten@bullestock.net>
2017-08-18 20:47:31 +0200

    Rudimentary motor control

--- ctrlr/main.cpp
+++ ctrlr/main.cpp
@@ -83,7 +83,13 @@ int main(int argc, char** argv)
 
     int powerL = 0;
     int powerR = 0;
-    
+
+    // The threshold at which the pivot action starts
+    // This threshold is measured in units on the Y-axis
+    // away from the X-axis (Y=0). A greater value will assign
+    // more of the joystick's range to pivot actions.
+    const int pivot = 35;
+
 	while (1)
 	{
         // if there is data ready
@@ -106,8 +112,47 @@ int main(int argc, char** argv)
                 radio.write(&frame.ticks, sizeof(frame.ticks));
 
                 radio.startListening();
+
                 cerr << "Sticks " << frame.left_x << " " << frame.left_y
                      << " " << frame.right_x << " " << frame.right_y << endl;
+                
+                const int max_power = 100;
+
+                const int max_range = 511;
+                const int rx = frame.right_x - max_range;
+                const int ry = frame.right_y - max_range;
+
+                cout << "RX " << rx << " RY " << ry << endl;
+
+                int nMotPremixL = 0;
+                int nMotPremixR = 0;
+                // Calculate Drive Turn output due to Joystick X input
+                if (ry >= 0)
+                {
+                    // Forward
+                    nMotPremixL = rx >= 0 ? max_range : max_range + rx;
+                    nMotPremixR = rx >= 0 ? max_range - rx : max_range;
+                }
+                else
+                {
+                    // Reverse
+                    nMotPremixL = rx >= 0 ? max_range - rx : max_range;
+                    nMotPremixR = rx >= 0 ? max_range : max_range + rx;
+                }
+
+                // Scale Drive output due to Joystick Y input (throttle)
+                nMotPremixL = nMotPremixL * ry/(max_range+1.0);
+                nMotPremixR = nMotPremixR * ry/(max_range+1.0);
+
+                // Now calculate pivot amount
+                // - Strength of pivot (nPivSpeed) based on Joystick X input
+                // - Blending of pivot vs drive (fPivScale) based on Joystick Y input
+                const auto nPivSpeed = rx;
+                const auto fPivScale = abs(ry) > pivot ? 0.0 : 1.0 - abs(ry)/float(pivot);
+
+                // Calculate final mix of Drive and Pivot and convert to motor PWM range
+                powerL = int(-((1.0-fPivScale)*nMotPremixL + fPivScale*(nPivSpeed))/float(max_range)*max_power);
+                powerR = int(-((1.0-fPivScale)*nMotPremixR + fPivScale*(-nPivSpeed))/float(max_range)*max_power);
             }
         }
         delay(10);
@@ -118,13 +163,8 @@ int main(int argc, char** argv)
             static_cast<uint8_t>(abs(powerL)),
             static_cast<uint8_t>(abs(powerR))
         };
-        cout << powerL << "/" << powerR << endl;
+        //cout << powerL << "/" << powerR << endl;
         if (write(i2c_device, data, sizeof(data)) != sizeof(data))
             cout << "Error writing I2C: " << strerror(errno) << endl;
-
-        ++powerL;
-        ++powerR;
-        if (powerL > 255)
-            powerL = 0;
     }
 }
