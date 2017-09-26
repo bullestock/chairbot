f7d7d70bf33b2839553908f75820426fd1e5e572
Torsten Martinsen <torsten@bullestock.net>
2017-09-26 19:23:37 +0000

    Add sound and lights control.

---
+++ ctrlr/signal.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdint.h>
+
+bool signal_init(int& i2c_device);
+
+bool signal_play_sound(int i2c_device, int sound_index);
+
+bool signal_control_lights(int i2c_device, bool on);
