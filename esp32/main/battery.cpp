a21d467057289f9c3b433b6e0ddd5bb18e2533dc
Torsten Martinsen <torsten@bullestock.net>
2019-11-18 21:25:01 +0100

    Add fudge factor.

--- esp32/main/battery.cpp
+++ esp32/main/battery.cpp
@@ -12,5 +12,6 @@ float Battery::read_voltage() const
 {
     auto adc_reading = adc1_get_raw((adc1_channel_t) ADC_CHANNEL_7);
     uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
-    return voltage * 0.010968279735;
+    printf("raw adc %d\n", (int) voltage);
+    return voltage * 0.010968279735 * 1.00413736036;
 }
