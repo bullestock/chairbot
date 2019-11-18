8cd84f1fcafdbedb6c6f8f0459340e5c55713569
Torsten Martinsen <torsten@bullestock.net>
2019-11-18 21:32:40 +0100

    Remove debug.

--- esp32/main/battery.cpp
+++ esp32/main/battery.cpp
@@ -12,6 +12,5 @@ float Battery::read_voltage() const
 {
     auto adc_reading = adc1_get_raw((adc1_channel_t) ADC_CHANNEL_7);
     uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
-    printf("raw adc %d\n", (int) voltage);
     return voltage * 0.010968279735 * 1.00413736036;
 }
