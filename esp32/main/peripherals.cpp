afe8f279dfc9ff070ff2ba09cb30f7b55607c328
Torsten Martinsen <torsten@bullestock.net>
2019-11-18 21:23:19 +0100

    Fix ADC attenuation setting.

--- esp32/main/peripherals.cpp
+++ esp32/main/peripherals.cpp
@@ -51,9 +51,9 @@ void init_peripherals()
 	ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
 
     adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten((adc1_channel_t) ADC_CHANNEL_7, ADC_ATTEN_DB_0);
+    adc1_config_channel_atten((adc1_channel_t) ADC_CHANNEL_7, ADC_ATTEN_DB_11);
     adc_chars = (esp_adc_cal_characteristics_t*) calloc(1, sizeof(esp_adc_cal_characteristics_t));
-    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_0, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
 }
 
 const int i2c_address = 5;
