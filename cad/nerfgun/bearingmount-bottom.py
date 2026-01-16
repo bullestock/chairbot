bc28f01e0a236e4cb9b1301be3fce556bbb38d6e
Torsten Martinsen <torsten@bullestock.net>
2026-01-16 20:05:41 +0100

    Increase height by 3 mm.

--- cad/nerfgun/bearingmount-bottom.py
+++ cad/nerfgun/bearingmount-bottom.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from build123d import *
 from ocp_vscode import *
 
-th = 2
+th = 2 + 3
 id = 5.5
 od = 12
 rr = 0.5
@@ -15,15 +15,19 @@ with BuildPart() as p:
         Circle(radius=od/2)
     extrude(amount=th + g_d)
     # hole
-    with BuildSketch():
+    with BuildSketch(p.faces().sort_by(Axis.Z).last):
         Circle(radius=id/2)
-    extrude(amount=th + g_d, mode=Mode.SUBTRACT)
+    extrude(amount=-th, mode=Mode.SUBTRACT)
+    # through hole
+    with BuildSketch(p.faces().sort_by(Axis.Z).last):
+        Circle(radius=2.7/2)
+    extrude(amount=-(th + g_d), mode=Mode.SUBTRACT)
     # slits
     with BuildSketch(p.faces().sort_by(Axis.Z).last):
         Rectangle(g_w, od)
         Rectangle(od, g_w)
     extrude(amount=-g_d, mode=Mode.SUBTRACT)
 
-show(p)
+show(p, reset_camera=Camera.KEEP)
 
 export_step(p.part, f'{Path(__file__).stem}.step')
