e9169fa1d140363a4acd280931ff451da7519182
Torsten Martinsen <torsten@bullestock.net>
2026-03-28 11:46:01 +0100

    Complete.

--- cad/nerfgun/counterweight.py
+++ cad/nerfgun/counterweight.py
@@ -11,6 +11,15 @@ E = 1.9
 F = 5.5
 H = 3 # max 8
 groove_len = 40
+groove_h = F+H
+
+lid_th = 2
+
+pocket_w = 30
+pocket_h = 11
+pocket_l = 48 + lid_th
+
+shell_th = 1.5
 
 pts = [
     ( 0, 0 ),
@@ -18,18 +27,37 @@ pts = [
     ( C, H ),
     ( C, H+E ),
     ( C+B-A, H+E ),
+    ( C+B-A, H+F ),
     ( C+B, H+F ),
     ( C+B, H ),
     ( D, H ),
+    ( D, 0 ),
 ]
 
-with BuildPart() as p:
-    with BuildSketch(Plane.YZ):
+for i in range(0, len(pts)):
+    pts[i] = ( pts[i][0] - D/2, pocket_h - pts[i][1] + 1 )
+
+with BuildPart() as cutout:
+    with BuildSketch(Plane.XZ.offset(11.5)):
         with BuildLine():
             Polyline(pts, close=True)
         make_face()
     extrude(amount=groove_len)
-    #fillet(p.edges().filter_by(Axis.Z), radius=1)
-    #fillet(p.edges().sort_by(Axis.Z)[-1], radius=1)
+    fillet(cutout.edges().filter_by(Axis.Y), radius=.5)
+
+with BuildPart() as p:
+    with BuildSketch(Plane.XZ):
+        RectangleRounded(
+            pocket_w + 2*shell_th,
+            pocket_h + 2*shell_th + groove_h,
+            1)
+    extrude(amount=pocket_l + shell_th)
+    fillet(p.edges().filter_by(Axis.X), radius=1)
+    with BuildSketch(p.faces().sort_by(Axis.Y).last):
+        with Locations((0, groove_h/2)):
+            RectangleRounded(pocket_w, pocket_h, 0.5)
+    extrude(amount=-pocket_l, mode=Mode.SUBTRACT)
+
+p.part -= cutout.part
 
 epilogue(p)
