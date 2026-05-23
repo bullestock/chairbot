58f88e0be17af5eeab74b66656b4a15b94d09c6e
Torsten Martinsen <torsten@bullestock.net>
2026-05-23 20:49:52 +0200

    Update for new PCB.

--- cad/pcbmount.py
+++ cad/pcbmount.py
@@ -6,22 +6,23 @@ import sys
 import re
 
 # Assumes SolidPython is in site-packages or elsewhwere in sys.path
-from solid import *
-from solid.utils import *
+from solid2 import *
+#from solid2.utils import *
 
-SEGMENTS = 32
+SEGMENTS = 500
+set_global_fn(100)
 
-sd = 7
+sd = 8
 th = 5
+hole_cc = 57.6278
+
 
 def stud():
     return up(th)(cylinder(d = sd, h = 2))
 
 def studhole():
-    inner = down(1)(cylinder(d = 2.5, h = 4+th)) + up(-0.1)(cylinder(d1 = 5, d2 = 2, h = 2))
-    ww = 4
-    c = translate([-ww/2, -ww, -1])(cube([ww, 2*ww, 20]))
-    return intersection()(c, inner)
+    inner = down(1)(cylinder(d = 4.2, h = 2*th))
+    return inner
 
 def assembly():
     s = stud()
@@ -31,12 +32,13 @@ def assembly():
     l = d + sd
     h1 = 3
     h = down(1)(cylinder(d = 3, h = th + 1)) + up(th - h1 + 0.1)(cylinder(d1 = 3, d2 = 7, h = h1))
-    studs = left(d/2)(s) + right(d/2)(s)
-    studholes = left(d/2)(sh) + right(d/2)(sh)
+    studs = left(hole_cc/2)(s) + right(hole_cc/2)(s)
+    studholes = left(hole_cc/2)(sh) + right(hole_cc/2)(sh)
     dd = d - 20
     holes = left(dd/2)(h) + right(dd/2)(h)
     return studs + translate([-l/2, -w/2, 0])(cube([l, 2*w, th])) - forward(w)(holes) - studholes
 
 if __name__ == '__main__':
     a = assembly()
-    scad_render_to_file(a, file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
+    a.save_as_scad()
+    #scad_render_to_file(a, file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
